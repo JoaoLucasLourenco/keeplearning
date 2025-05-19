e4cc5d678365cc6ada0501950e97da3655e78497
João Lucas Lourenço <118781114+JoaoLucasLourenco@users.noreply.github.com>
2025-05-19 17:26:58 +0000

    Header Home Page

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,11 +6,3 @@ const PORT = process.env.SERVER_PORT || 3030;
 app.listen(PORT, () => {
   console.log(`Server running in port: ${PORT}`);
 });
-
-app.get("/", (req, res) => {
-  res.send("requisição na rota /");
-});
-
-app.get("/usuarios", (req, res) => {
-  res.send("requisição na rota /usuarios");
-});
